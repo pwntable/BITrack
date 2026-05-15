6f8d0bdc14f5329adf3e304d432f1f767c148500
pwntable <khaiul7245@gmail.com>
2026-05-16 02:19:45 +0800

    fix: enforce absolute github pages paths for pwa

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'bitrack-v1';
 const ASSETS = [
-  './',
-  './manifest.json',
-  './icon-192x192.png',
-  './icon-512x512.png'
+  '/BITrack/',
+  '/BITrack/manifest.json',
+  '/BITrack/icon-192x192.png',
+  '/BITrack/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
